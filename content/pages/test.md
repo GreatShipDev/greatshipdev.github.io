47f51a32112c2fa538b1668bc7191047bdb56ab5
emily.lerner <emily@greatship.dev>
2023-06-11 16:09:39 -0400

    initial commit using pelican repo to generate pages - see if it plays nice with github pages

---
+++ content/pages/test.md
@@ -0,0 +1,2 @@
+Title: About Us
+Great Ship Development utilizes a fail-fast mentality to manifest creative products that rock the boat. 
---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+AUTHOR = 'emily@greatship.dev'
+SITENAME = 'Great Ship Development'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'EST'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Our Github', 'https://github.com/GreatShipDev'),
+         )
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
